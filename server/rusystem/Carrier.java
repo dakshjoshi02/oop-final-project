8603ff97a67af9d94ba0e3916c16ef4e18f3a3d2
alanshensihao <alanshensihao@gmail.com>
2023-07-17 11:28:14 -0400

    RFPort and Carrier and Frequency Band update

--- server/rusystem/Carrier.java
+++ server/rusystem/Carrier.java
@@ -1,18 +1,19 @@
 package rusystem;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Carrier {
 
     private static int globalId = 1;
     private final int id;
-    private ArrayList<String> RFPorts; // TODO: type of rf ports
-    private String frequencyBand;
+    private HashSet<RURFPorts> RFPorts;
+    private HashSet<FrequencyBand> frequencyBand;
     private double transmittingPower;
 
     public Carrier() {
         this.id = globalId;
         ++globalId;
+        RFPorts = new HashSet<RURFPorts>();
     }
 
     public boolean createLteCarrier() {
