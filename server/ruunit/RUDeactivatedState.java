51ee4ecc91b071484b0c28d7fb1260997c3b1c9c
Shriya Satish <11shriya@gmail.com>
2023-07-17 16:28:41 -0400

    RU STate Activation and Deactivation methods
    Added Functionalities to RU State

--- server/ruunit/RUDeactivatedState.java
+++ server/ruunit/RUDeactivatedState.java
@@ -3,21 +3,21 @@ package ruunit;
 public class RUDeactivatedState implements RUState {
     @Override
     public void setup() {
-
+        System.out.println("setup");
     }
 
     @Override
     public void deactivate() {
-
+        System.out.println("deactivate");
     }
 
     @Override
     public void activate() {
-
+        System.out.println("activate");
     }
 
     @Override
     public void release() {
-
+        System.out.println("release");
     }
 }
